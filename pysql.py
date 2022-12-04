0858a25fd8fbdf9acc058c3536fabcacbbe5f31e
Rad Ali <rad.ali@polymtl.ca>
2022-12-04 16:28:54 -0500

    Added argument parser

--- pysql.py
+++ pysql.py
@@ -1,10 +1,36 @@
 import pymysql.cursors
+import argparse
+
+#Parse query arguments
+parser = argparse.ArgumentParser(description='Instance setup.')
+parser.add_argument('--query', help='Run query')
+args = parser.parse_args()
+
+#Verify what type of operation is being requested 
+sql = ""
+queryType = ""
+if args.query is None:
+    sql = "SELECT * FROM actor WHERE actor_id <=1"
+    queryType= "SELECT"
+else:
+    sql = args.query
+    if sql.upper().startswith("SELECT") > -1: 
+        queryType= "SELECT"
+    elif sql.upper().startswith("INSERT") > -1: 
+        queryType= "INSERT"
 
 masterIP = ""
+slave0IP = ""
+slave1IP = ""
+slave2IP = ""
 
-# Read Master IP from environment variables file
+# Read IPs from environment variables file
 with open("env_variables.txt", 'r') as file:
-    masterIP = file.readlines()[1][10:].replace("\n","")
+    lines = file.readlines()
+    masterIP = lines[1][10:].replace("\n","")
+    slave0IP = lines[2][10:].replace("\n","")
+    slave1IP = lines[3][10:].replace("\n","")
+    slave2IP = lines[4][10:].replace("\n","")
 
 # Connect to the database
 connection = pymysql.connect(host=masterIP,
@@ -17,7 +43,6 @@ connection = pymysql.connect(host=masterIP,
 with connection:
     with connection.cursor() as cursor:
         # Read a single record
-        sql = "SELECT * FROM actor WHERE actor_id <10"
         cursor.execute(sql)
         result = cursor.fetchall()
         print(result)
