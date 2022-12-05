e488493a1ad5f5edb6a5b45704d817b6ca588eee
Rad Ali <rad.ali@polymtl.ca>
2022-12-05 00:11:28 -0500

    Initial commit of querying script

---
+++ run_query.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+source ./env_variables.txt
+
+#EXAMPLE OF QUERY : "SELECT * FROM actor WHERE actor_id <=10"
+
+ssh -o "StrictHostKeyChecking no" -i "$PRIVATE_KEY_FILE" ubuntu@"$PROXY_IP" << HERE
+cd Advanced-Cloud-Computing-Final-Project/
+python3 pysql.py --query "$1"
+HERE
