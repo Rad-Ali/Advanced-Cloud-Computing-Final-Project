2821ac5301f848248de213944731b58de2d48f11
Rad Ali <rad.ali@polymtl.ca>
2022-12-07 15:13:04 -0500

    Implemented sshtunnel to allow address binding

--- pysql.py
+++ pysql.py
@@ -1,13 +1,13 @@
 import pymysql.cursors
 import argparse
 import random
+from sshtunnel import open_tunnel
 
-def create_connection(IP,bindAddress=None):
+def create_connection(IP):
     """Create connection with MySQL database
 
     Args:
         IP (str): instance IP to connect to
-        bindAddress (str) : IP address of bound slave node
 
     Returns:
         connection: connection to instance's database
@@ -16,11 +16,45 @@ def create_connection(IP,bindAddress=None):
                              user='test',
                              password='pass',
                              database='sakila',
-                             cursorclass=pymysql.cursors.DictCursor,
-                             bind_address=bindAddress
+                             cursorclass=pymysql.cursors.DictCursor
                              )
 
     return connection
+
+def execute_sql(connection, sql):
+    """Execute the sql query
+
+    Args:
+        connection: connection to the SQL database
+        sql (str) : SQL query string
+    Returns:
+        result: result of sql query
+    """
+    with connection:
+            with connection.cursor() as cursor:
+                # Read a single record
+                cursor.execute(sql)
+                result = cursor.fetchall()
+                return result
+
+def open_bound_tunnel(bindAddress, sql):
+    """Opens ssh tunnel to the required slave and executes the sql query
+
+    Args:
+        bindAddress (str) : IP address of the slave
+        sql (str) : SQL query string
+    Returns:
+        result: result of sql query
+        connection : connection to the database
+    """
+    with open_tunnel(
+    (bindAddress, 22),
+    ssh_username="ubuntu",
+    ssh_pkey="private_key_LAB3_KEY.pem",
+    remote_bind_address=(masterIP, 3306)
+    ) as server:
+        connection = create_connection(masterIP)
+        return execute_sql(connection, sql), connection
 
 def read_ping(filename):
     """Read the ping value of slave from textfile
@@ -49,16 +83,16 @@ group.add_argument('-c', '--customized', action='store_true', default=False)
 args = parser.parse_args()
 print(args)
 #Verify what type of operation is being requested 
-sql = ""
+query = ""
 queryType = ""
 if args.query is None:
-    sql = "SELECT * FROM actor WHERE actor_id <=1"
+    query = "SELECT * FROM actor WHERE actor_id <=1"
     queryType= "SELECT"
 else:
-    sql = args.query
-    if sql.upper().startswith("SELECT") > -1: 
+    query = args.query
+    if query.upper().startswith("SELECT") > -1: 
         queryType= "SELECT"
-    elif sql.upper().startswith("INSERT") > -1: 
+    elif query.upper().startswith("INSERT") > -1: 
         queryType= "INSERT"
 
 masterIP = ""
@@ -76,34 +110,41 @@ with open("env_variables.txt", 'r') as file:
 
 slaveList = [slave0IP, slave1IP, slave2IP]
 
-bindAddress = None                                           # Direct hit
-if args.random: bindAddress = slaveList[random.randrange(3)] # Random
-if args.customized :                                         # Customized
-    slave0Time = read_ping("slave0.txt")
-    slave1Time = read_ping("slave1.txt")
-    slave2Time = read_ping("slave2.txt")
-    bindAddress = slave0IP
-    if slave1Time <  slave0Time: bindAddress = slave1IP
-    if slave2Time < slave1Time or slave2Time < slave0Time: bindAddress = slave2IP
-
-
 # Connect to the database with respect to query type
 if queryType == "SELECT":
-    connection = create_connection(masterIP,bindAddress)
-
-    # Test connection with sakila database with a select query
-    with connection:
-        with connection.cursor() as cursor:
-            # Read a single record
-            cursor.execute(sql)
-            result = cursor.fetchall()
-            print(result)
+    # Execute SQL SELECT query depending on type of implementation                                      
+    if args.random:                                                 # Random
+        print(open_bound_tunnel(slaveList[random.randrange(3)], query))
+    elif args.customized :                                          # Customized
+        slave0Time = read_ping("slave0.txt")
+        slave1Time = read_ping("slave1.txt")
+        slave2Time = read_ping("slave2.txt")
+        boundAddress = slave0IP
+        if slave1Time <  slave0Time: boundAddress = slave1IP
+        if slave2Time < slave1Time or slave2Time < slave0Time: boundAddress = slave2IP
+        print(open_bound_tunnel(boundAddress, query))
+    else :                                                          # Direct hit
+        connection = create_connection(masterIP)
+        print(execute_sql(connection, query))
+    # Connect to the database with respect to query type
 elif queryType == "INSERT":
-    connection = create_connection(masterIP)
-    with connection:
-        with connection.cursor() as cursor:
-            # Read a single record
-            cursor.execute(sql)
-            result = cursor.fetchall()
-            print(result)
-        connection.commit()
+    # Execute SQL INSERT query depending on type of implementation                                      
+    if args.random:                                                 # Random
+        result, con = open_bound_tunnel(slaveList[random.randrange(3)], query)  
+        print(result)
+        con.commit()      
+    elif args.customized :                                          # Customized
+        slave0Time = read_ping("slave0.txt")
+        slave1Time = read_ping("slave1.txt")
+        slave2Time = read_ping("slave2.txt")
+        boundAddress = slave0IP
+        if slave1Time <  slave0Time: boundAddress = slave1IP
+        if slave2Time < slave1Time or slave2Time < slave0Time: boundAddress = slave2IP
+        result, con = open_bound_tunnel(boundAddress, query)
+        print(result)
+        con.commit()
+    else :                                                          # Direct hit
+        con = create_connection(masterIP)
+        print(execute_sql(con, query))
+        con.commit()
+    
