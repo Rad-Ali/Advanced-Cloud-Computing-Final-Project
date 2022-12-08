d79ae940acc622648b1e488ec647b95baed9a434
Rad Ali <rad.ali@polymtl.ca>
2022-12-08 15:55:56 -0500

    Added proxy call comments

--- run_query.sh
+++ run_query.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
 
+# Import all the necessary IP and DNS addresses
 source ./env_variables.txt
 
 #EXAMPLE OF QUERY : "SELECT * FROM actor WHERE actor_id <=10"
 
+# What is done here :
+#    - Enter python script directory
+#    - Run query with 1 or 2 arguments
 ssh -o "StrictHostKeyChecking no" -i "$PRIVATE_KEY_FILE" ubuntu@"$PROXY_IP" << HERE
 cd Advanced-Cloud-Computing-Final-Project/
 python3 pysql.py --query "$1" $2
