efe6050361f4615527c26a1ed18556be9e71f69f
Rad Ali <rad.ali@polymtl.ca>
2022-12-08 15:59:47 -0500

    Added bemchmarking comments

--- benchmarks.sh
+++ benchmarks.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 
+# Import all the necessary IP and DNS addresses
 source env_variables.txt
 
+# What is done here :
+#    - Download and install sysbench on master instance
+#    - Prepare benchmark
+#    - Run benchmark
 ssh -o "StrictHostKeyChecking no" -i "$PRIVATE_KEY_FILE" ubuntu@"$MASTER_IP" << HERE
 set -x 
 source /etc/profile.d/mysqlc.sh
@@ -10,6 +15,10 @@ sysbench oltp_read_write --table-size=1000000 --mysql-db="sakila" --mysql-host="
 sysbench oltp_read_write --table-size=1000000 --mysql-host="$MASTER_IP" --num-threads=16 --max-time=10 --max-requests=0 --mysql-db="sakila" --mysql-user="test" --mysql-password="pass" run
 HERE
 
+# What is done here :
+#    - Download and install sysbench on standalone instance
+#    - Prepare benchmark
+#    - Run benchmark
 ssh -o "StrictHostKeyChecking no" -i "$PRIVATE_KEY_FILE" ubuntu@"$STANDALONE_IP" << HERE
 set -x 
 source /etc/profile.d/mysqlc.sh
